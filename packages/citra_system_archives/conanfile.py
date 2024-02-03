b820a312bbaf9f96ee99d7e2277d2ee00d396729
Tony Wasserka <neobrain@users.noreply.github.com>
2024-02-03 11:32:48 +0100

    citra_system_archives: Add NVer archive

--- packages/citra_system_archives/conanfile.py
+++ packages/citra_system_archives/conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile, tools
-from conan.tools.files import chdir, copy, get, replace_in_file
+from conan.tools.files import chdir, copy, get, replace_in_file, save
 from conan.tools.gnu import Autotools
 
 import os
@@ -24,8 +24,12 @@ class Conan(ConanFile):
         replace_in_file(self, 'country_list/country-archive.py', 'd = tempfile.mkdtemp()', 'os.mkdir("romfs"); writeToArchive("romfs", archive); return')
         replace_in_file(self, 'mii/mii.py', 'd = tempfile.mkdtemp()', 'os.mkdir("romfs"); Build(inPath, "romfs/CFL_Res.dat"); return')
 
+        save(self, 'nver/run.sh', "#!/bin/sh\n./../common/romfs.py -c romfs 00000000.app.romfs")
+        os.chmod('nver/run.sh', 0o777)
+        save(self, 'nver/romfs/version.bin', '\0\0\0\0\0\0\0\0')
+
     def build(self):
-        for archive_name in ['bad_word_list', 'country_list', 'mii', 'shared_font']:
+        for archive_name in ['bad_word_list', 'country_list', 'mii', 'nver', 'shared_font']:
             with chdir(self, os.path.join(self.source_folder, archive_name)):
                 self.run('./run.sh')
                 self.run("makerom -rsf ../info.rsf -o 00000000.app")
@@ -34,4 +38,5 @@ class Conan(ConanFile):
         copy(self, "00000000.app", src=os.path.join(self.source_folder, 'bad_word_list'), dst=os.path.join(self.package_folder, '000400db/00010302/content'))
         copy(self, "00000000.app", src=os.path.join(self.source_folder, 'country_list'), dst=os.path.join(self.package_folder, '0004009b/00010402/content'))
         copy(self, "00000000.app", src=os.path.join(self.source_folder, 'mii'), dst=os.path.join(self.package_folder, '0004009b/00010202/content'))
+        copy(self, "00000000.app", src=os.path.join(self.source_folder, 'nver'), dst=os.path.join(self.package_folder, '000400db/00016102/content'))
         copy(self, "00000000.app", src=os.path.join(self.source_folder, 'shared_font'), dst=os.path.join(self.package_folder, '0004009b/00014002/content'))
